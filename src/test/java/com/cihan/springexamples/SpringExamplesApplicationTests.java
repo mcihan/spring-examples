2105da811a012405064d5b5333850ddd523e69c9
Cihan Dogan <cihan.dogan@TRIS2LT007.tr.asseco-see.local>
2020-05-18 07:03:55 +0300

    init

---
+++ src/test/java/com/cihan/springexamples/SpringExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cihan.springexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
